3a1a31f20bd67e4ad8a5416035bc10f97adeca63
Yuki Kimura <kashima.no5@gmail.com>
2023-11-08 02:15:10 +0900

    feat: storybookの導入

---
+++ src/components/ui/button.stories.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./button";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Button",
+  component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    children: "Button",
+  },
+};
